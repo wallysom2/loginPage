3382ff1cbb50d04a46224a1c45b80edd6b811a62
matheus <wallysom2@gmail.com>
2024-05-18 22:57:34 -0300

    refactor: Remove dns.setDefaultResultOrder('verbatim') from vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,6 @@
 import { defineConfig } from 'vite';
 import eslintPlugin from '@nabla/vite-plugin-eslint';
 import react from '@vitejs/plugin-react-swc';
-import dns from 'dns';
-
-dns.setDefaultResultOrder('verbatim');
 
 export default defineConfig({
   server: {
@@ -12,12 +9,8 @@ export default defineConfig({
   },
   build: {
     rollupOptions: {
-      plugins: [],
-      input: {
-        main: '/path/to/main.tsx',
-      },
+      plugins: []
     },
   },
   plugins: [react(), eslintPlugin()],
-  resolve: {},
 });
