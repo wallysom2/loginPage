c049ae47e218ebdd3e91b1226d639f293e9f30b2
matheus <wallysom2@gmail.com>
2024-05-18 23:25:47 -0300

    refactor: Remove unused _redirects file

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react'
 import eslintPlugin from '@nabla/vite-plugin-eslint';
-import { resolve } from 'path'
-import { copyFileSync } from 'fs'
 
 export default defineConfig({
   server: {
@@ -16,11 +14,5 @@ export default defineConfig({
   },
   plugins: [
     react(), eslintPlugin (),
-    {
-      name: 'copy-redirects',
-      buildEnd() {
-        copyFileSync(resolve(__dirname, 'public/_redirects'), resolve(__dirname, 'dist/_redirects'))
-      }
-    }
-  ],
+    ],
 });
